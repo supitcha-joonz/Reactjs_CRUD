571959e590074061d4da7003dfdfbf10055e9d8b
Supitcha Jampathong (NTL) <CF303956@tidlor.com>
2022-06-28 15:57:38 +0700

    add navbar CRUD Link

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,13 +5,14 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
+          {/* <IconButton
             size="large"
             edge="start"
             color="inherit"
@@ -19,10 +20,13 @@ export default function Navbar() {
             sx={{ mr: 2 }}
           >
             <MenuIcon />
-          </IconButton>
-          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-            CRUD
-          </Typography>
+          </IconButton> */}
+          <Link to="/" style={{ textDecoration: 'none' , color: 'white'}}>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+              CRUD
+            </Typography>
+          </Link>
+
         </Toolbar>
       </AppBar>
     </Box>
